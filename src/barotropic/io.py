263f146afe78d7614c1ae35716725cc8ebcc91c4
Christopher Polster <chpolste@tuta.io>
2024-05-31 22:59:08 +0200

    WIP: rewrite of from_dataset

--- src/barotropic/io.py
+++ src/barotropic/io.py
@@ -1,7 +1,6 @@
-"""Input/output functions (**work in progress**)."""
-
 from collections import abc
 import datetime as dt
+import warnings
 
 import numpy as np
 
@@ -117,8 +116,35 @@ def as_dataset(states, fields=("pv", "u", "v"), concat_dim="time"):
     })
 
 
-def from_dataset(dataset, names=None, grid_kwargs=None):
+_VAR_NAMES = {
+    "lon": ["longitude", "lon"],
+    "lat": ["latitude", "lat"],
+    "time": ["time"],
+    "u": ["u", "ugrd", "eastward_wind"],
+    "v": ["v", "vgrd", "northward_wind"],
+    "rv": ["vo", "relv", "vorticity", "relative_vorticity"],
+    # PV is usually not absolute vorticity and only considered as a last resort
+    "pv": ["avo", "absv", "absolute_vorticity", "pv"],
+}
+
+def _extract_var(ds, names, override=None):
+    if override is not None:
+        return ds[name]
+    for name in names:
+        if name in ds:
+            return ds[name]
+    return None
+
+def from_dataset(ds, names=None, grid_kwargs=None):
     """Load states from u and v components in an xarray Dataset
+
+    Parameters:
+        dataset (:py:class:`xarray.Dataset`): TODO
+        names (dict | None): TODO
+        grid_kwargs (dict | None): TODO
+
+    Returns:
+        :py:class:`.StateList`
     
     If automatic detection of grid and winds based on the coordinate
     attributes of the dataset fails, the association of `lat`, `lon`,
@@ -127,73 +153,97 @@ def from_dataset(dataset, names=None, grid_kwargs=None):
     on the dataset coordinates. Additional arguments for the `Grid`
     instanciation can be specified with the `grid_kwargs` argument.
     """
-    # TODO also accept fields of absolute/relative vorticity
-    # TODO write proper error messages
-    # Dataset should have 3 dimensions: time, lat, lon
-    assert len(dataset.dims) == 3
-    # Initialize mapping of coordinate names
-    var_map = { "lat": None, "lon": None, "time": None, "u": None, "v": None }
-    # Iterate through all coordinates of the dataset and try to detect
-    # usable fields based on metadata
-    for name, var in dataset.variables.items():
-        lname = var.attrs["long_name"].lower() if "long_name" in var.attrs else None
-        units = var.attrs["units"].lower() if "units" in var.attrs else None
-        if lname == "time":
-            var_map["time"] = name
-        # Verify that latitude and longitude is given in degrees
-        elif lname == "longitude":
-            assert "degree" in units and "east" in units
-            var_map["lon"] = name
-        elif lname == "latitude":
-            assert "degree" in units and "north" in units
-            var_map["lat"] = name
-        # Verify that wind components are given in m/s
-        elif lname is not None and "wind" in lname:
-            assert "m s**-1" in units or "m s^-1" in units or "m/s" in units
-            if "u component" in lname or "zonal" in lname:
-                var_map["u"] = name
-            if "v component" in lname or "meridional" in lname:
-                var_map["v"] = name
-    # Override mapping with given name map argument
-    if names is not None:
-        var_map.update(names)
-    # Verify that every required variable has been found
-    assert all(var is not None for var in var_map.values())
-    # Extract latitude and longitude coordinates and verify basic
-    # properties (non-emptyness, shape)
-    lon = dataset[var_map["lon"]].values
-    lat = dataset[var_map["lat"]].values
-    assert lat.size > 0
-    assert lon.size > 0
-    assert lat.shape[0] % 2 == 1
-    assert lat.shape[0] - 1 == lon.shape[-1] // 2
-    # Verify that latitudes go from north pole to south pole, longitudes
-    # from west to east and grid is regular
-    dlats = np.diff(lat, axis= 0).flatten()
-    dlons = np.diff(lon, axis=-1).flatten()
-    dlat = dlats[0]
+    # TODO allow path for ds, open here with xr.open_dataset
+
+    # Extract coordinates, respect user override if provided
+    if names is None:
+        names = dict()
+    var_map = {
+        k: _extract_var(ds, ns, override=names.get(k, None))
+        for k, ns in _VAR_NAMES.items()
+    }
+
+    # Verify longitude coordinate
+    if var_map["lon"] is None:
+        raise ValueError("required coordinate 'lon' not detected, please provide name")
+    lon = var_map["lon"]
+    if "units" in lon.attrs:
+        units = lon.attrs["units"]
+        if not "degree" in units and "east" in units:
+            warnings.warn(f"expected 'degrees_east' as units of lon, but found '{units}'")
+    assert lon.ndim == 1, "lon coordinate must be one-dimensional"
+    assert lon.size > 0, "lon coordinate is empty"
+    dlons = np.diff(lon)
     dlon = dlons[0]
-    assert dlat < 0
-    assert dlon > 0
-    assert np.isclose(-dlat, dlon)
-    assert np.isclose(dlat, dlats).all()
-    assert np.isclose(dlon, dlons).all()
-    # Instanciate shared Grid that matches dataset coordinates
+    assert np.isclose(dlon, dlons).all(), "lon spacing is not regular"
+    assert dlon > 0, "lon coordinate must be increasing (W to E)"
+    # TODO test peridodicity
+
+    # Verify latitude coordinate
+    if var_map["lat"] is None:
+        raise ValueError("required coordinate 'lat' not detected, please provide name")
+    lat = var_map["lat"] # also ensures extracted var is a coordinate
+    if "units" in lat.attrs:
+        units = var_map["lat"].attrs["units"]
+        if not "degree" in units and "north" in units:
+            warnings.warn(f"expected 'degrees_north' as units of lat, but found '{units}'")
+    assert lat.ndim == 1, "lat coordinate must be one-dimensional"
+    assert lat.size > 0, "lat coordinate is empty"
+    assert lat.size % 2 == 1, "lat coordinate must have odd number of grid points"
+    assert lat.size - 1 == lon.size // 2, f"grid of shape [{lat.size}, {lon.size}] is not regular"
+    dlats = np.diff(lat)
+    dlat = dlats[0]
+    assert np.isclose(dlat, dlats).all(), "lat spacing is not regular"
+    assert dlat < 0, "lat coordinate must be decreasing (N to S)"
+    assert np.isclose(dlat, -dlon), "grid is not regular: lat spacing {dlat}, lon spacing {dlon}"
+
+    # Instanciate Grid that matches dataset coordinates
     grid_kwargs = {} if grid_kwargs is None else grid_kwargs
     grid = Grid(resolution=dlon, **grid_kwargs)
-    # For every time in the dataset, extract the wind fields and create
-    # a State from them
+
+    # Consider multiple options to construct states:
+    # States from horizontal wind components
+    if var_map["u"] is not None and var_map["v"] is not None:
+        # TODO verify u (NaN, plausibility, units, ...)
+        # TODO verify v (NaN, plausibility, units, ...)
+        # Verify that wind components are given in m/s
+        #lname = var.attrs["long_name"].lower() if "long_name" in var.attrs else None
+        #units = var.attrs["units"].lower() if "units" in var.attrs else None
+        #if lname is not None and "wind" in lname:
+        #    assert "m s**-1" in units or "m s^-1" in units or "m/s" in units
+        #    if "u component" in lname or "zonal" in lname:
+        #        var_map["u"] = name
+        #    if "v component" in lname or "meridional" in lname:
+        #        var_map["v"] = name
+        as_state = lambda t, d: State.from_wind(grid, t, d[var_map["u"]], d[var_map["v"]])
+    # States from relative vorticity
+    elif var_map["rv"] is not None:
+        # TODO verify rv (NaN, plausibility, units, ...)
+        as_state = lambda t, d: State.from_vorticity(grid, t, d[var_map["vo"]])
+    # States from absolute vorticity
+    elif var_map["pv"] is not None:
+        # TODO verify pv (NaN, plausibility, units, ...)
+        as_state = lambda t, d: State(grid, t, pv=d[var_map["vo"]])
+    else:
+        raise ValueError(
+            "no fields for state construction detected, please specify"
+            " names of either 'u' and 'v', 'rv' or 'pv'."
+        )
+
+    # Dataset should have 3 dimensions: time, lat, lon
+    assert len(ds.dims) == 3 # TODO not necessary, but warn of flattening
+    # TODO rewrite with more construction options
     states = []
-    for time in dataset[var_map["time"]].values:
-        data = dataset.sel({ var_map["time"]: time }, drop=True)
+    for time in var_map["time"].values:
+        data = ds.sel({ var_map["time"].name: time }, drop=True)
         # Verify that coordinates are in the right order
-        assert tuple(data.coords) == (var_map["lon"], var_map["lat"])
+        assert tuple(data.coords) == (var_map["lon"].name, var_map["lat"].name)
         # Convert numpy datetime type into a regular datetime instance
         # https://stackoverflow.com/questions/13703720/
         if isinstance(time, np.datetime64):
             time = dt.datetime.utcfromtimestamp((time - np.datetime64(0, "s")) / np.timedelta64(1, "s"))
         states.append(
-            State.from_wind(grid, time, data[var_map["u"]].values, data[var_map["v"]].values)
+            State.from_wind(grid, time, data[var_map["u"].name].values, data[var_map["v"].name].values)
         )
-    return states
+    return StateList(states)
 
